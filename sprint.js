a533d50fda3df29b2edc1e8b444755745f9018f6
Sid N <snallu@amplify.com>
2015-03-14 12:31:04 -0400

    Setup signing in with google accounts.

--- sprint.js
+++ sprint.js
@@ -17,7 +17,9 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        ServiceConfiguration.configurations.remove({
+            service: "google"
+        });
+    });
 }
